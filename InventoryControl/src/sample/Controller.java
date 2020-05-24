44b2b80c8107f615fcfcf7fbb4692c6f05a91ce7
hansolo580 <47253266+hansolo580@users.noreply.github.com>
2020-05-24 12:14:15 -0400

    Wireframes in place for all views. Implemented appropriate radio button displays for part management screens.

--- InventoryControl/src/sample/Controller.java
+++ InventoryControl/src/sample/Controller.java
@@ -9,14 +9,29 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    private boolean internalPart;
+
     @FXML private Button IMSAddPartButton;
+    @FXML private Label partLabel;
+
+    @FXML void AddPartButtonInternal(ActionEvent event) {
+        internalPart = true;
+        partLabel.setText("Machine ID");
+    }
+    @FXML void AddPartButtonOutsourced(ActionEvent event) {
+        internalPart = false;
+        partLabel.setText("Company Name");
+    }
 
     /**
      * When this method is called, it will change the scene to AddPartView
@@ -31,6 +46,20 @@ public class Controller implements Initializable {
         window.setScene(newPartScene);
         window.show();
     }
+
+    // This changes to ModifyPartView
+
+    public void changeScreenModifyPart(ActionEvent event) throws IOException {
+        Parent modifyPartParent = FXMLLoader.load(getClass().getResource("ModifyPartView.fxml"));
+        Scene modifyPartScene = new Scene(modifyPartParent);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(modifyPartScene);
+        window.show();
+    }
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
